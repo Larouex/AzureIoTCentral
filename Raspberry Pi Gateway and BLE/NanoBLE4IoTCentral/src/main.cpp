82384eb163a3c33029188ac8ccfbb33e390ec43e
Larry W Jordan Jr <larouex@gmail.com>
2020-06-09 07:25:31 -0700

    Web Basics and Navigation In Place #13

--- Raspberry Pi Gateway and BLE/NanoBLE4IoTCentral/src/main.cpp
+++ Raspberry Pi Gateway and BLE/NanoBLE4IoTCentral/src/main.cpp
@@ -5,26 +5,46 @@
 #include <Arduino.h>
 #include <ArduinoBLE.h>
 #include <string>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
 #define onboard 13
+#define ONE_WIRE_BUS 2
 
 bool connected = false;
 
-/*
-  The Values are used when we do a check on the battery charge. The
-  charge is tested when the DPDT Momentary Switch is pressed and the
-  charging/voltage regulator is bypassed and the battery is then
-  directly connected.
-*/
+/* --------------------------------------------------------------------------
+    The Values are used when we do a check on the battery charge. The
+    charge is tested when the DPDT Momentary Switch is pressed and the
+    charging/voltage regulator is bypassed and the battery is then
+    directly connected.
+   -------------------------------------------------------------------------- */
 #define red_light_pin 11
 #define green_light_pin 10
 #define blue_light_pin 9
 
+/* --------------------------------------------------------------------------
+    This is he pin we will be reading for the digital input from the DHT22
+    #
+   -------------------------------------------------------------------------- */
+OneWire oneWire(ONE_WIRE_BUS);
+
+// Pass our oneWire reference to Dallas Temperature. 
+DallasTemperature sensors(&oneWire);
+
+// arrays to hold device addresses
+DeviceAddress insideThermometer, outsideThermometer;
+
+/* --------------------------------------------------------------------------
+    Previous Basttery Level Monitors
+   -------------------------------------------------------------------------- */
 int oldBatteryLevel = 0;  // last battery level reading from analog input
 long previousMillis =  0;  // last time the battery level was checked, in ms
 
-double currentTemperature = 67.4;
-double currentHumidity = 75.1;
-int telemetryFrequency = 5;
+float currentTemperature = 0.0;
+float currentHumidity = 0.0;
+float currentBarometer = 0.0;
+int telemetryFrequency = 500;
 
 const int buttonPin = 4; // set buttonPin to digital pin 4
 
@@ -40,12 +60,67 @@ BLEByteCharacteristic telemetryTemperatureCharacteristic(telemetryServiceId, BLE
 BLEByteCharacteristic telemetryHumidityCharacteristic(telemetryServiceId, BLERead);
 BLEByteCharacteristic telemetryFrequencyCharacteristic(telemetryServiceId, BLERead | BLEWrite);
 
-/*
-  Function to set the RGB LED to the color of the battery charge
-  * Green >=50%
-  * Yellow <= 49% && >=10%
-  * Red <=9%
-*/
+// function to print a device address
+void printAddress(DeviceAddress deviceAddress)
+{
+  for (uint8_t i = 0; i < 8; i++)
+  {
+    if (deviceAddress[i] < 16) Serial.print("0");
+    Serial.print(deviceAddress[i], HEX);
+  }
+}
+
+// function to print the temperature for a device
+void printTemperature(DeviceAddress deviceAddress)
+{
+  float tempC = sensors.getTempC(deviceAddress);
+  Serial.print("Temp C: ");
+  Serial.print(tempC);
+  Serial.print(" Temp F: ");
+  Serial.print(DallasTemperature::toFahrenheit(tempC));
+}
+
+void printAlarms(uint8_t deviceAddress[])
+{
+  char temp;
+  temp = sensors.getHighAlarmTemp(deviceAddress);
+  Serial.print("High Alarm: ");
+  Serial.print(temp, DEC);
+  Serial.print("C/");
+  Serial.print(DallasTemperature::toFahrenheit(temp));
+  Serial.print("F | Low Alarm: ");
+  temp = sensors.getLowAlarmTemp(deviceAddress);
+  Serial.print(temp, DEC);
+  Serial.print("C/");
+  Serial.print(DallasTemperature::toFahrenheit(temp));
+  Serial.print("F");
+}
+
+// main function to print information about a device
+void printData(DeviceAddress deviceAddress)
+{
+  Serial.print("Device Address: ");
+  printAddress(deviceAddress);
+  Serial.print(" ");
+  printTemperature(deviceAddress);
+  Serial.println();
+}
+
+void checkAlarm(DeviceAddress deviceAddress)
+{
+  if (sensors.hasAlarm(deviceAddress))
+  {
+    Serial.print("ALARM: ");
+    printData(deviceAddress);
+  }
+}
+
+/* --------------------------------------------------------------------------
+    Function to set the RGB LED to the color of the battery charge
+      * Green >=50%
+      * Yellow <= 49% && >=10%
+      * Red <=9%
+   -------------------------------------------------------------------------- */
 void SetBatteryColor(int red_light_value, int green_light_value, int blue_light_value)
  {
   Serial.print("Red: ");
@@ -81,10 +156,11 @@ void BatteryCheck(int level) {
   return;
 }
 
+/* --------------------------------------------------------------------------
+    Read the current voltage level on the A0 analog input pin.
+    This is used here to simulate the charge level of a battery.
+   -------------------------------------------------------------------------- */
 void UpdateBatteryLevel() {
-  /* Read the current voltage level on the A0 analog input pin.
-     This is used here to simulate the charge level of a battery.
-  */
   int battery = analogRead(A0);
   int batteryLevel = map(battery, 0, 1023, 0, 100);
 
@@ -95,7 +171,35 @@ void UpdateBatteryLevel() {
     oldBatteryLevel = batteryLevel;           // save the level for next comparison
     BatteryCheck(batteryLevel);
   }
+}
 
+/* --------------------------------------------------------------------------
+    Update the telemtry for Temperature
+   -------------------------------------------------------------------------- */
+void UpdateTemperature(DeviceAddress deviceAddress) {
+  
+  sensors.requestTemperatures();
+  Serial.print("Temperature is: "); 
+  float temperature = sensors.getTempCByIndex(0);
+  Serial.print("Temperature Read = ");
+  Serial.println(DallasTemperature::toFahrenheit(temperature));
+/*
+  Serial.print("Current Temperature = ");
+  Serial.println(DallasTemperature::toFahrenheit(currentTemperature));
+
+  float temperature = sensors.getTempC(deviceAddress);
+  Serial.print("Temperature Read = ");
+  Serial.println(DallasTemperature::toFahrenheit(temperature));
+
+  if (currentTemperature != temperature)
+  {
+    telemetryTemperatureCharacteristic.writeValue(DallasTemperature::toFahrenheit(temperature));
+    Serial.print("Temperature = ");
+    Serial.println(DallasTemperature::toFahrenheit(temperature));
+    currentTemperature = temperature;
+  }
+  */
+  delay(telemetryFrequency);
 }
 
 void blePeripheralConnectHandler(BLEDevice central) {
@@ -127,16 +231,20 @@ void setup() {
   pinMode(green_light_pin, OUTPUT);
   pinMode(blue_light_pin, OUTPUT);
   pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(ONE_WIRE_BUS, INPUT_PULLUP);  
 
   SetBatteryColor(255, 0, 0); // Red
   delay(1000);
 
   Serial.begin(9600);    // initialize serial communication
   while (!Serial);
-
+  
+  // Start up the library
+  sensors.begin();
+  
   // begin initialization
   if (!BLE.begin()) {
-    Serial.println("starting BLE failed!");
+    Serial.println("Initializing BLE has Failed!");
     while (1);
   }
 
@@ -155,14 +263,19 @@ void setup() {
   BLE.addService(batteryService); // Add the battery service
   batteryCharacteristic.writeValue(oldBatteryLevel); // set initial value for this characteristic
 
-  // add the telemetry service
+  // add the telemetry services
   BLE.setAdvertisedService(telemetryService);
   telemetryService.addCharacteristic(telemetryTemperatureCharacteristic);
   telemetryService.addCharacteristic(telemetryHumidityCharacteristic);
   telemetryService.addCharacteristic(telemetryFrequencyCharacteristic);
   BLE.addService(telemetryService);
+
+  // get our initial measurements
+  UpdateTemperature(insideThermometer);
+
   telemetryTemperatureCharacteristic.writeValue(currentTemperature);
   telemetryHumidityCharacteristic.writeValue(currentHumidity);
+
   telemetryFrequencyCharacteristic.writeValue(telemetryFrequency);
 
   // assign event handlers for connected, disconnected to peripheral
@@ -190,6 +303,8 @@ void loop() {
         previousMillis = currentMillis;
         UpdateBatteryLevel();
       }
-  }
+
+      UpdateTemperature(insideThermometer);
+    }
 }
 
